a170bc7689ed2f789cea96fab226aac7961662e8
k.savostin <k.savostin@inventos.ru>
2023-10-02 14:07:07 +0300

    feat: add css to chat page
    - code refactor;
    - add css to chat page;

--- moose_messenger/Server.py
+++ moose_messenger/Server.py
@@ -1,38 +1,46 @@
 from datetime import datetime
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 from moose_messenger.accounts_functions import check_user_access, create_user_account
 
 app = Flask(__name__)
+app.secret_key = '73870e7f-634d-433b-946a-8d20132bafac'
 
 @app.route('/')
 def index():
    return render_template("login.html", context = {"message": ""})
 
+@app.route('/chatting')
+def chatting():
+    return render_template("main_page.html", context=session.get("data"))
+
+
 @app.route('/Access_checking',  methods=['POST'])
-def check_access():
+def Access_checking():
     email = request.form['email']
     password = request.form['password']
     access = check_user_access(email, password)
     if access:
-        return render_template("main_page.html", context={"username": access})
+        session['data'] = {"username": access}
+        return redirect(url_for('chatting'))
     else:
         return render_template("login.html", context = {"message": "Неверный логин или пароль"})
         
 
 @app.route('/Create_account',  methods=['POST'])
-def a():
+def Create_account():
     email = request.form['email']
     username = request.form['username']
     password = request.form['password']
     ans = create_user_account(email, password, username)
     if ans:
-        return render_template("main_page.html", context={"username": username} )
+        session['data'] = {"username": username}
+        return redirect(url_for('chatting'))
     else:
         return render_template("register.html", context = {"message": "Учётная запись с таким логином уже существует"})
         
 @app.route('/Send_message',  methods=['POST'])
-def b():
+def Send_message():
     mes = json.loads(request.data.decode(encoding="utf-8"))
     with open ("message_hystory.json") as file:
         messages = json.load(file)
@@ -47,7 +55,7 @@ def b():
     return("f")
 
 @app.route('/Get_messages',  methods=['POST'])
-def get_messages():
+def Get_messages():
     try:
         with open("message_hystory.json") as file:
             messages = json.load(file)
@@ -56,9 +64,13 @@ def get_messages():
         return []
 
 @app.route('/register',  methods=['GET'])
-def success():
+def register():
    if request.method == 'GET':
        return render_template('register.html', context = {"message": ""})
    
+@app.route("/design/main_page_style.css")
+def get_design():
+    return ('design/main_page_style.css')
+
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port="5000")
